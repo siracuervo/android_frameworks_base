6a83000f19baf31e333fc2389a4d048417bde286
Kelly <mushroomhead52e@gmail.com>
2012-08-15 20:17:38 -0400

    Add lockscreen rotation as an optional rotation setting (1/2)
    This will allow the user to select wether or not to use the
    rotation settings for the lockscreen.  This is dependent
    upon the rotation setting being checked.
    
    Forward port from cm-10.2, adjusted for 4.4
    
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: Ibd7c69b0af1e0a7d91b985f2b21ea0b5bc619a70
    JIRA: CYAN-339
    
    Conflicts:
    	packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2324,13 +2324,6 @@ public final class Settings {
         public static final String ACCELEROMETER_ROTATION = "accelerometer_rotation";
 
         /**
-         * Control whether the accelerometer will be used to change lockscreen
-         * orientation.  If 0, it will not be used; if 1, it will be used by default.
-         * @hide
-         */
-        public static final String LOCKSCREEN_ROTATION = "lockscreen_rotation";
-
-        /**
          * Control the type of rotation which can be performed using the accelerometer
          * if ACCELEROMETER_ROTATION is enabled.
          * Value is a bitwise combination of
@@ -2342,6 +2335,13 @@ public final class Settings {
          * @hide
          */
         public static final String ACCELEROMETER_ROTATION_ANGLES = "accelerometer_rotation_angles";
+
+        /**
+         * Control whether the accelerometer will be used to change lockscreen
+         * orientation.  If 0, it will not be used; if 1, it will be used by default.
+         * @hide
+         */
+        public static final String LOCKSCREEN_ROTATION = "lockscreen_rotation";
 
         /**
          * Default screen rotation when no other policy applies.
