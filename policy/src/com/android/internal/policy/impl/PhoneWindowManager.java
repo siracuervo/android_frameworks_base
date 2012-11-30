9c46bb73a4e117315105c05c71a208964c6bcc96
Danesh M <daneshm90@gmail.com>
2012-11-29 22:26:21 -0500

    Allow applications to override power key
    Allows user to take use power key to take pictures/videos eg
    
    Change-Id: I85a1e1761199f4604672be42a3a5005227f5451a

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -4188,6 +4188,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
 
             case KeyEvent.KEYCODE_POWER: {
+                if ((mTopFullscreenOpaqueWindowState.getAttrs().flags
+                        & WindowManager.LayoutParams.PREVENT_POWER_KEY) != 0){
+                    return result;
+                }
                 result &= ~ACTION_PASS_TO_USER;
                 if (down) {
                     if (mExpandedDesktopMode == 0) {
