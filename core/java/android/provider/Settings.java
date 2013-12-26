c95aeb9d8954e873cb40ef0f7bab63808adfe397
liquid0624 <rjbirnesser@gmail.com>
2013-12-25 23:27:54 -0500

    Custom system animations (1/2)
    Ported from here: http://gerrit.aokp.co/#/c/12304/
    
    Thanks to Steve Spear
    
    Change-Id: I38bc7b83edaff3c455f0dfe3ea5ede54a20ae07c
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/cm_strings.xml
    	core/res/res/values/symbols.xml
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	core/res/res/values/liquid_symbols.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3054,6 +3054,25 @@ public final class Settings {
          */
         public static final String ACTIVITY_RESOLVER_USE_ALT = "activity_resolver_use_alt";
 
+	    /**
+         * Custom System Animations
+         * @hide
+         */
+        public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {  
+                "activity_open",
+                "activity_close",
+                "task_open",  
+                "task_close",  
+                "task_to_front",
+                "task_to_back",  
+                "wallpaper_open",  
+                "wallpaper_close",  
+                "wallpaper_intra_open",  
+                "wallpaper_intra_close",  
+        };  
+        public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
+        public static final String ANIMATION_CONTROLS_NO_OVERRIDE = "animation_controls_no_override";
+
         /**
          * ListView Animations
          * 0 == None
@@ -3142,12 +3161,6 @@ public final class Settings {
          * @hide
          */
         public static final String NAVIGATION_BAR_SHOW = "navigation_bar_show";
-
-        /**
-         * Wether navigation bar is on landscape on the bottom or on the right
-         * @hide
-         */
-        public static final String NAVIGATION_BAR_CAN_MOVE = "navigation_bar_can_move";
 
         /**
          * Navigation bar height when it is on protrait
@@ -3289,7 +3302,6 @@ public final class Settings {
 
         /**
          * Should the non-intrsive incall ui be used
-         *
          * @hide
          */
         public static final String NON_INTRUSIVE_INCALL = "non_intrusive_incall";
