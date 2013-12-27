5a6305a53514a30cc5829a340159303a1e93988f
liquid0624 <rjbirnesser@gmail.com>
2013-12-27 02:21:20 -0500

    Custom carrier text (2/2) same as the other commit
    Change-Id: I34e8de1921dce740454101020a1f259cb89f02e2
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	policy/src/com/android/internal/policy/impl/keyguard/CarrierText.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3307,6 +3307,13 @@ public final class Settings {
         public static final String NON_INTRUSIVE_INCALL = "non_intrusive_incall";
 
         /**
+         * Holds the text for the Carrier label. An empty string
+         * will bring the default text back.
+         * @hide
+         */
+        public static final String CUSTOM_CARRIER_LABEL = "custom_carrier_label";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
