SlimRoms/frameworks_base

QS: add theme switcher tile (TRDS) 1/2
this adds a theme switcher tile

normal click: turn on/off dark/light theme
longpress: switch between the auto modes (manual/light sensor/twilight)

Change-Id: I62b8e11b5714c179ccbf1c1e341b47c84da0fb56

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -41,6 +41,7 @@ import static com.android.internal.util.slim.QSConstants.TILE_SCREENTIMEOUT;
 import static com.android.internal.util.slim.QSConstants.TILE_SETTINGS;
 import static com.android.internal.util.slim.QSConstants.TILE_SLEEP;
 import static com.android.internal.util.slim.QSConstants.TILE_SYNC;
+import static com.android.internal.util.slim.QSConstants.TILE_THEME;
 import static com.android.internal.util.slim.QSConstants.TILE_TORCH;
 import static com.android.internal.util.slim.QSConstants.TILE_USBTETHER;
 import static com.android.internal.util.slim.QSConstants.TILE_USER;
@@ -88,6 +89,7 @@ import com.android.systemui.quicksettings.RingerModeTile;
 import com.android.systemui.quicksettings.ScreenTimeoutTile;
 import com.android.systemui.quicksettings.SleepScreenTile;
 import com.android.systemui.quicksettings.SyncTile;
+import com.android.systemui.quicksettings.ThemeTile;
 import com.android.systemui.quicksettings.ToggleLockscreenTile;
 import com.android.systemui.quicksettings.TorchTile;
 import com.android.systemui.quicksettings.UsbTetherTile;
@@ -241,6 +243,8 @@ public class QuickSettingsController {
                 qs = new MusicTile(mContext, this);
             } else if (tile.equals(TILE_REBOOT)) {
                 qs = new RebootTile(mContext, this);
+            } else if (tile.equals(TILE_THEME)) {
+                qs = new ThemeTile(mContext, this);
             }
 
             if (qs != null) {

siracuervo/android_frameworks_base

Conflicts:
	packages/SystemUI/res/values/liquid_strings.xml
	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -41,6 +41,7 @@ import static com.android.internal.util.liquid.QSConstants.TILE_SCREENTIMEOUT;
 import static com.android.internal.util.liquid.QSConstants.TILE_SETTINGS;
 import static com.android.internal.util.liquid.QSConstants.TILE_SLEEP;
 import static com.android.internal.util.liquid.QSConstants.TILE_SYNC;
+import static com.android.internal.util.liquid.QSConstants.TILE_THEME;
 import static com.android.internal.util.liquid.QSConstants.TILE_TORCH;
 import static com.android.internal.util.liquid.QSConstants.TILE_USBTETHER;
 import static com.android.internal.util.liquid.QSConstants.TILE_USER;
@@ -48,6 +49,7 @@ import static com.android.internal.util.liquid.QSConstants.TILE_VOLUME;
 import static com.android.internal.util.liquid.QSConstants.TILE_WIFI;
 import static com.android.internal.util.liquid.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.liquid.QSConstants.TILE_REBOOT;
+
 
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
@@ -88,6 +90,7 @@ import com.android.systemui.quicksettings.RingerModeTile;
 import com.android.systemui.quicksettings.ScreenTimeoutTile;
 import com.android.systemui.quicksettings.SleepScreenTile;
 import com.android.systemui.quicksettings.SyncTile;
+import com.android.systemui.quicksettings.ThemeTile;
 import com.android.systemui.quicksettings.ToggleLockscreenTile;
 import com.android.systemui.quicksettings.TorchTile;
 import com.android.systemui.quicksettings.UsbTetherTile;
@@ -241,6 +244,8 @@ public class QuickSettingsController {
                 qs = new MusicTile(mContext, this);
             } else if (tile.equals(TILE_REBOOT)) {
                 qs = new RebootTile(mContext, this);
+            } else if (tile.equals(TILE_THEME)) {
+                qs = new ThemeTile(mContext, this);
             }
 
             if (qs != null) {