fd799975cfbeefd88fb4ffbd3568665ebc286619
liquid0624 <rjbirnesser@gmail.com>
2014-01-13 00:27:43 -0500

    Fix custom carrier and wifi label derp

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
@@ -1127,10 +1127,10 @@ public class NetworkController extends BroadcastReceiver implements DemoMode {
             }
         }
 
-        if (customLabel != null && customLabel.length() > 0) {	
-            combinedLabel = customLabel;	
-            mobileLabel = customLabel;	
-            wifiLabel = customLabel;	
+        if (customLabel != null && customLabel.length() > 0) {
+            combinedLabel = customLabel;
+            mobileLabel = customLabel;
+            // wifiLabel = customLabel;
         }
 
         if (DEBUG) {
