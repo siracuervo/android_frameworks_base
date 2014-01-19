4a7e3f499c8e8aae0b40509dfacc7796bf1ef2bd
maxwen <max.weninger@gmail.com>
2014-01-19 23:06:31 +0100

    [1/2] Frameworks: Wakelock Blocker
    Change-Id: Id4fd1d8da6ed40517094a5a156f7c6fea622e985
    
    Conflicts:
    	core/java/android/os/IPowerManager.aidl
    	core/java/android/provider/Settings.java
    	services/java/com/android/server/power/PowerManagerService.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2861,6 +2861,18 @@ public final class Settings {
         public static final String POWER_MENU_CONFIG = "power_menu_config";
 
         /**
+         * Enable blocking wakelock
+         * @hide
+         */
+        public static final String WAKELOCK_BLOCKING_ENABLED = "wakelock_blocking_enabled";
+
+        /**
+         * List of wakelock blocks selected
+         * @hide
+         */
+        public static final String WAKELOCK_BLOCKING_LIST = "wakelock_blocking_list";
+
+        /**
          * Text color for advanced power menu
          *
          * @hide
