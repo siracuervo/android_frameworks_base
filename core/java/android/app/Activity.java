3f1991f31c8ae38e34c192ccf33fadac987f905f
jkl5616 <jkl5616@gmail.com>
2014-01-23 14:29:34 +0900

    Initial Chameleon Engine
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- core/java/android/app/Activity.java
+++ core/java/android/app/Activity.java
@@ -1103,6 +1103,9 @@ public class Activity extends ContextThemeWrapper
         if (DEBUG_LIFECYCLE) Slog.v(TAG, "onResume " + this);
         getApplication().dispatchActivityResumed(this);
         mCalled = true;
+        Intent intent = new Intent("tb.newactivity");
+        intent.putExtra("packagename",getPackageName());
+        sendBroadcast(intent);
     }
 
     /**
--- packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
@@ -32,6 +32,7 @@ import android.widget.LinearLayout;
 
 import com.android.systemui.R;
 import com.android.systemui.statusbar.policy.NetworkController;
+import com.android.systemui.statusbar.phone.PhoneStatusBar;
 
 // Intimately tied to the design of res/layout/signal_cluster_view.xml
 public class SignalClusterView
@@ -54,6 +55,7 @@ public class SignalClusterView
     private boolean mIsAirplaneMode = false;
     private int mAirplaneIconId = 0;
     private String mWifiDescription, mMobileDescription, mMobileTypeDescription;
+    private PhoneStatusBar mStatusBar;
 
     ViewGroup mWifiGroup, mMobileGroup;
     ImageView mWifi, mMobile, mWifiActivity, mMobileActivity, mMobileType, mAirplane;
@@ -102,6 +104,10 @@ public class SignalClusterView
         if (DEBUG) Log.d(TAG, "NetworkController=" + nc);
         mNC = nc;
     }
+   
+    public void setStatusBar(PhoneStatusBar mStatusBar) {
+        this.mStatusBar = mStatusBar;
+    }
 
     @Override
     protected void onAttachedToWindow() {
@@ -120,6 +126,13 @@ public class SignalClusterView
         mAirplane       = (ImageView) findViewById(R.id.airplane);
 
         apply();
+        
+        mStatusBar.addIcon(mWifi);
+		mStatusBar.addIcon(mMobile);
+		mStatusBar.addIcon(mWifiActivity);
+		mStatusBar.addIcon(mMobileActivity); 
+		mStatusBar.addIcon(mMobileType); 
+		mStatusBar.addIcon(mAirplane);
     }
 
     @Override
