f6af5f6bba286ef2e0b02e85e18033fd461c6cd3
liquid0624 <rjbirnesser@gmail.com>
2014-01-24 16:35:57 -0500

    Revert "Use boost hints on dexopt"
    This reverts commit 8b25b9b6b2a3b9b0daca8308b06de0ebdfe30e29.

--- services/java/com/android/server/pm/PackageManagerService.java
+++ services/java/com/android/server/pm/PackageManagerService.java
@@ -110,7 +110,6 @@ import android.os.Looper;
 import android.os.Message;
 import android.os.Parcel;
 import android.os.ParcelFileDescriptor;
-import android.os.PowerManager;
 import android.os.Process;
 import android.os.RemoteException;
 import android.os.SELinux;
@@ -220,8 +219,6 @@ public class PackageManagerService extends IPackageManager.Stub {
     private static final String INSTALL_PACKAGE_SUFFIX = "-";
 
     private static final int THEME_MAMANER_GUID = 1300;
-
-    private final PowerManager mPm;
 
     static final int SCAN_MONITOR = 1<<0;
     static final int SCAN_NO_DEX = 1<<1;
@@ -1137,8 +1134,6 @@ public class PackageManagerService extends IPackageManager.Stub {
         mSettings.addSharedUserLPw("com.tmobile.thememanager", THEME_MAMANER_GUID,
                 ApplicationInfo.FLAG_SYSTEM|ApplicationInfo.FLAG_PRIVILEGED);
 
-        mPm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
-        
         mAppOps = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
         String separateProcesses = SystemProperties.get("debug.separate_processes");
         if (separateProcesses != null && separateProcesses.length() > 0) {
@@ -3976,7 +3971,6 @@ public class PackageManagerService extends IPackageManager.Stub {
     private int performDexOptLI(PackageParser.Package pkg, boolean forceDex, boolean defer,
             HashSet<String> done) {
         boolean performed = false;
-        mPm.cpuBoost(1500000);
         if (done != null) {
             done.add(pkg.packageName);
             if (pkg.usesLibraries != null) {
