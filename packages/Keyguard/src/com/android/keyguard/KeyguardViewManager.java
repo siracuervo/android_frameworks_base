05d8bcf484080c5b7261698a8bee30d99ab69aa7
Anu6is <curtis.mayers@gmail.com>
2014-01-28 16:04:00 -0200

    Fix: Correct possible null pointer when enabling see through lockscreen
    Change-Id: Iee3b51504a1d233c63ef9d4a97f9cf4d51f42694
    PS2: Remove unused variable
    
    Conflicts:
    	packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
@@ -157,11 +157,11 @@ public class KeyguardViewManager {
         public void onChange(boolean selfChange) {
             setKeyguardParams();
             updateSettings();
-            try {
-                mViewManager.updateViewLayout(mKeyguardHost, mWindowLayoutParams);
-            } catch(IllegalArgumentException e) {
-                // Call yo mom call yo dad!
+            if (mKeyguardHost == null) {
+                maybeCreateKeyguardLocked(shouldEnableScreenRotation(), false, null);
+                hide();
             }
+            mViewManager.updateViewLayout(mKeyguardHost, mWindowLayoutParams);
         }
     }
 
@@ -633,8 +633,11 @@ public class KeyguardViewManager {
 
             int flags = WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
                     | WindowManager.LayoutParams.FLAG_LAYOUT_INSET_DECOR
-                    | WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN
-                    | WindowManager.LayoutParams.FLAG_SHOW_WALLPAPER;
+                    | WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN;
+
+            if (!isSeeThroughEnabled()) {
+                flags |= WindowManager.LayoutParams.FLAG_SHOW_WALLPAPER;
+            }
 
             if (!mNeedsInput) {
                 flags |= WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM;
