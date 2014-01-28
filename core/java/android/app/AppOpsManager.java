ccdd28c8889b8ebcd9544443b9a0e56b705b5d98
Ethan Chen <intervigil@gmail.com>
2014-01-28 13:48:25 -0800

    Hide additional AppOps function
    Change-Id: Ife06bea56a2907acc9a7ab024e9b9aa30835652f
    
    Conflicts:
    	core/java/android/app/AppOpsManager.java

--- core/java/android/app/AppOpsManager.java
+++ core/java/android/app/AppOpsManager.java
@@ -1335,6 +1335,7 @@ public class AppOpsManager {
         return com.android.internal.R.drawable.stat_notify_privacy_guard_off;
     }
 
+    /** @hide */
     public void resetCounters() {
         try {
             mService.resetCounters();
