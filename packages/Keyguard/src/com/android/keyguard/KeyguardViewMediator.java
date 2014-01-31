3902b36e1217375925fc2caa9ad9bda5167de7fc
David96 <xd_e-mail@gmx.de>
2014-02-01 00:17:37 +0100

    Custom lockscreen wallpaper [1/2]
    Conflicts:
    	packages/Keyguard/src/com/android/keyguard/KeyguardViewMediator.java

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewMediator.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewMediator.java
@@ -545,6 +545,10 @@ public class KeyguardViewMediator {
         mKeyguardViewManager.setBackgroundBitmap(bmp);
     }
 
+    public void setWallpaper(Bitmap bmp) {
+        mKeyguardViewManager.setWallpaper(bmp);
+    }
+
     /**
      * Let us know that the system is ready after startup.
      */
