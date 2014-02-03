a5e0fa8f3d4a4f47bfd151e801a99a83ba4808ba
liquid0624 <rjbirnesser@gmail.com>
2014-02-02 22:36:58 -0500

    fb: finally fix carrier lable shows on first boot sometimes
    Issue:
    - boot up
    - directly open via fast QS pulldown QS
    - sometimes carrier label is shown
    
    Expected behaviour: should not show
    
    Fix: Visibility is directly set when the settings observer is registered first time.
    If no update was done on the carrier label before the user pulled QS down the first time
    it showed up. Vary rare case that this can happen
    but should be handled. To fix it do not set the visibility directly and call the
    updateCarrierAndWifiLabelVisibility(boolean) method to handle the correct visibility.
    
    As well last cleanup on notification shorcuts...move the check if it is
    active or not to the visbility method itself. Way cleaner
    
    And remove the previous attempt to fix the above described issue due
    it did not fix it at all
    
    Change-Id: I901beb987c21fe0cdb7a5d9f16f850f4297c15f4

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -592,8 +592,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 mHideLabels = Settings.System.getIntForUser(resolver,
                         Settings.System.NOTIFICATION_HIDE_LABELS, 0, UserHandle.USER_CURRENT);
                 updateCarrierMargin(mHideLabels == 3);
-                mCarrierAndWifiView.setVisibility(
-                        (mHideLabels != 3) ? View.VISIBLE : View.INVISIBLE);
+                if (mHideLabels == 3) {
+                    mCarrierAndWifiViewVisible = false;
+                    mCarrierAndWifiView.setVisibility(View.INVISIBLE);
+                }
+                updateCarrierAndWifiLabelVisibility(false);
             }
             updateBatteryIcons();
             showClock(true);
@@ -1115,7 +1118,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         updateBatteryIcons();
 
         mNetworkController.setListener(this);
-        updateCarrierAndWifiLabelVisibility(true);
 
         return mStatusBarView;
     }
@@ -1160,10 +1162,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         lp.softInputMode = WindowManager.LayoutParams.SOFT_INPUT_STATE_UNCHANGED
         | WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING;
         return lp;
-    }
-
-    void onBarViewDetached() {
-        // WindowManagerImpl.getDefault().removeView(mStatusBarWindow);
     }
 
     @Override
@@ -2232,9 +2230,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mNotificationPanel.postDelayed(new Runnable() {
             public void run() {
                 updateCarrierAndWifiLabelVisibility(false);
-                if (mNotificationShortcutsIsActive) {
-                    updateNotificationShortcutsVisibility(true);
-                }
+                updateNotificationShortcutsVisibility(true);
             }
         }, FLIP_DURATION - 150);
         mNotificationPanelIsOpen = true;
@@ -2316,9 +2312,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mScrollView.setScaleX(-percent);
             mNotificationButton.setVisibility(View.GONE);
             updateCarrierAndWifiLabelVisibility(false);
-            if (mNotificationShortcutsIsActive) {
-                updateNotificationShortcutsVisibility(true);
-            }
+            updateNotificationShortcutsVisibility(true);
         } else { // settings side
             mFlipSettingsView.setScaleX(percent);
             mFlipSettingsView.setVisibility(View.VISIBLE);
