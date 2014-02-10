f7090dd5d6e403ab8d2ea24f9f5d9ed966293805
Lars Greiss <kufikugel@googlemail.com>
2014-02-10 22:28:25 +0100

    Frameworks: EdgeGestureService give abbility to reduce the left and right trigger height if IME shows up
    This patch gives the abbility that features can set the IME_CONTROL flag in addition to the position flags
    to enable reducing trigger heights on the left and right edge to avoid conflicts with swipe keyboard.
    
    Although the inputfilter lets come presses to the application trough the normal user behaviour is to start keyboard swiping
    before they really even pressed the char button which results in conflicts with whatever feature is using the left and right
    edge as trigger.
    
    Features who want to use it need to set IME_CONTROL flag. As well if notification drawer was pulled down or global menu is
    shown on top of the IME keyboard the original size gets restored till they are dismissed/closed
    
    This patch adds only the support for it.
    
    PS
    - Fix small logic derp in BaseStatusBar
    PS
    - follow Josues suggestion and make the trigger a bit bigger
    
    Change-Id: I51804d6e0768cc7770a6e52fac454dbad0ab632b

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -212,6 +212,8 @@ public abstract class BaseStatusBar extends SystemUI implements
     private PieController mPieController;
     protected NavigationBarOverlay mNavigationBarOverlay;
 
+    private EdgeGestureManager mEdgeGestureManager;
+
     // UI-specific methods
 
     /**
@@ -496,11 +498,15 @@ public abstract class BaseStatusBar extends SystemUI implements
     }
 
     private void initPieController() {
+        if (mEdgeGestureManager == null) {
+            mEdgeGestureManager = EdgeGestureManager.getInstance();
+        }
         if (mNavigationBarOverlay == null) {
             mNavigationBarOverlay = new NavigationBarOverlay();
         }
         if (mPieController == null) {
-            mPieController = new PieController(mContext, this, mNavigationBarOverlay);
+            mPieController = new PieController(
+                    mContext, this, mEdgeGestureManager, mNavigationBarOverlay);
             addNavigationBarCallback(mPieController);
         }
     }
@@ -511,6 +517,12 @@ public abstract class BaseStatusBar extends SystemUI implements
             mPieController.attachContainer();
         } else {
             mPieController.detachContainer(false);
+        }
+    }
+
+    public void setOverwriteImeIsActive(boolean enabled) {
+        if (mEdgeGestureManager != null) {
+            mEdgeGestureManager.setOverwriteImeIsActive(enabled);
         }
     }
 
