ee7fee1cd19a94587e36ad6cb13ebbdedc91d31b
liquid0624 <rjbirnesser@gmail.com>
2014-02-12 04:28:10 -0500

    Revert "FW: LS Notification: Fix FC party when Lockscreen Notifications are disabled"
    This reverts commit 634c27b62ab6e60989eba5c90a6989324ed3585e.

--- packages/Keyguard/src/com/android/keyguard/NotificationHostView.java
+++ packages/Keyguard/src/com/android/keyguard/NotificationHostView.java
@@ -282,9 +282,7 @@ public class NotificationHostView extends FrameLayout {
         mNotificationMinRowHeight = mContext.getResources().getDimensionPixelSize(R.dimen.notification_row_min_height);
         mNotificationManager = INotificationManager.Stub.asInterface(
                 ServiceManager.getService(Context.NOTIFICATION_SERVICE));
-        if (NotificationViewManager.config != null) {
-            mDynamicWidth = NotificationViewManager.config.dynamicWidth;
-        }
+        mDynamicWidth = getResources().getBoolean(R.bool.config_lnDynamicWidth);
     }
 
     @Override
