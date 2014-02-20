3812b8f49f617c9c3829a05998d9c70d24db720f
liquid0624 <rjbirnesser@gmail.com>
2014-02-19 23:20:48 -0500

    Port SmoothProgressBar: User Custom ProgressBar Setting[2/2]
    Sample : https://github.com/castorflex/SmoothProgressBar
    http://antoine-merle.com/images/smoothprogressbar_all.gif
    
    thanks Dokdo Project
    
    Change-Id: If476fb2a4fa9a988c73364107555b5e7624dea53
    Signed-off-by: hany <hany.alsamman@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4345,7 +4345,72 @@ public final class Settings {
          * @hide
          */
         public static final String LOCKSCREEN_BLUR_BEHIND = "lockscreen_blur_behind";
+
+        /**
+         * Radius of the blur lockscreen background
+         * @hide
+         */
         public static final String LOCKSCREEN_BLUR_RADIUS = "lockscreen_blur_radius";
+
+	    /**
+	     * SMOOTH PROGRESS BAR Mirror
+	     * @hide
+	     */
+        public static final String PROGRESSBAR_MIRROR = "progressbar_mirror";
+
+	    /**
+	     * SMOOTH PROGRESS BAR Reverse
+	     * @hide
+	     */
+        public static final String PROGRESSBAR_REVERSE = "progressbar_reverse";
+
+	    /**
+	     * SMOOTH PROGRESS BAR Speed
+	     * @hide
+	     */
+        public static final String PROGRESSBAR_SPEED = "progressbar_speed";
+
+        /**
+	     * SMOOTH PROGRESS BAR Width
+	     * @hide
+	     */
+        public static final String PROGRESSBAR_WIDTH = "progressbar_width";
+
+        /**
+	     * SMOOTH PROGRESS BAR Length
+	     * @hide
+	     */
+        public static final String PROGRESSBAR_LENGTH = "progressbar_length";
+
+        /**
+	     * SMOOTH PROGRESS BAR Count
+	     * @hide
+	     */
+        public static final String PROGRESSBAR_COUNT = "progressbar_count";
+
+        /**
+	     * SMOOTH PROGRESS BAR Color_1
+	     * @hide
+	     */
+        public static final String PROGRESSBAR_COLOR_1 = "progressbar_color_1";
+
+        /**
+	     * SMOOTH PROGRESS BAR Color_2
+	     * @hide
+	     */
+        public static final String PROGRESSBAR_COLOR_2 = "progressbar_color_2";
+
+        /**
+	     * SMOOTH PROGRESS BAR Color_3
+	     * @hide
+	     */
+        public static final String PROGRESSBAR_COLOR_3 = "progressbar_color_3";
+
+        /**
+	     * SMOOTH PROGRESS BAR Color_4
+	     * @hide
+	     */
+        public static final String PROGRESSBAR_COLOR_4 = "progressbar_color_4";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
